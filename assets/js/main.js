f087900331723917124bde7fc3d2e5b1073162e2
contact@audiopeak.net <contact@audiopeak.net>
2018-09-09 16:46:59 +0100

    Updated Contact and 76 other files

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-/*MobileOverlay*/
-(function($) {
-    "use strict";
-    jQuery(document).ready(function() {
-        jQuery("#toggle").click(function() {
-            "fixed" != jQuery("body").css("position") ? jQuery("body").css("position", "fixed") : jQuery("body").css("position", "relative"), jQuery(this).toggleClass("active"), jQuery(".overlay").toggleClass("open")
-        })
-    });
-})(jQuery);
